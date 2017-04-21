d69a1fb9dd1bb4c90190fbb1b0332afb0a8cefe7
Etienne Dldc <etienne.dldc@outlook.fr>
2017-04-22 01:07:05 +0200

    Rename Files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+
 /* eslint-disable no-unused-vars */
-import Basic from './Basic'
-import OneRender from './OneRender'
-import DisplaySize from './DisplaySize'
-import Percent from './Percent'
-import Inject from './Inject'
-import Advanced from './Advanced'
+import Basic from './01_Basic'
+import OneRender from './02_OneRender'
+import DisplaySize from './03_DisplaySize'
+import Percent from './04_Percent'
+import Inject from './05_Inject'
+import Advanced from './06_Advanced'
 /* eslint-enable */
 
 ReactDOM.render(
-  <Advanced />,
+  <Basic />,
   document.getElementById('root')
 )
