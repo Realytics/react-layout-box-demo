2f66ec14d7fb156eb41ccbc4d331369fac9c7578
Etienne Dldc <etienne.dldc@outlook.fr>
2017-04-22 22:31:40 +0200

    Style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { LayoutContainer, LayoutBox, InjectLayout } from './LayoutBox'
 
+const boxStyle = (lighness) => ({ background: `hsl(300, 41%, ${lighness}%)`, padding: 10, fontSize: '2em' })
+
 const StupidComponent = (props) => (
-  <p>{`${props.layout.width.value} x ${props.layout.height.value}`}</p>
+  <span>Size : {`${props.layout.width.value} x ${props.layout.height.value}`}</span>
 )
 const StupidComponentWithLayout = InjectLayout(StupidComponent)
 
 const App = () => (
   <LayoutContainer width={window.innerWidth} height={window.innerHeight}>
-    <LayoutBox height={70} style={{ background: 'RosyBrown' }}>TopBar</LayoutBox>
+    <LayoutBox height={70} style={boxStyle(70)}>TopBar</LayoutBox>
     <LayoutBox top={70}>
-      <LayoutBox width={200} style={{ background: 'BurlyWood' }}>Left Menu</LayoutBox>
-      <LayoutBox left={200} style={{ background: 'MistyRose' }}>
+      <LayoutBox width={200} style={boxStyle(60)}>Left Menu</LayoutBox>
+      <LayoutBox left={200} style={boxStyle(50)}>
         <StupidComponentWithLayout />
       </LayoutBox>
     </LayoutBox>
