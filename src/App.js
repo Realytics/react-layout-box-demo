2de97f76eb2dd075e7325c3e1d8fe1ae13b60939
Etienne Dldc <etienne.dldc@outlook.fr>
2017-04-22 22:10:00 +0200

    Step 5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,92 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { LayoutContainer, LayoutBox } from './LayoutBox'
+import throttle from 'lodash/throttle'
 
-const App = () => (
-  <h1>Hello Meetup React !</h1>
-)
+/**
+ * RenderCounter
+ */
+class RenderCounter extends Component {
+  constructor() {
+    super()
+    this.renderCount = 0
+  }
+  render() {
+    this.renderCount++
+    return (
+      <span>{this.renderCount}</span>
+    )
+  }
+}
+
+/**
+ * Content
+ */
+class Content extends Component {
+  constructor() {
+    super()
+    this.renderCount = 0
+    this.state = {
+      clickCount: 0
+    }
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.state.clickCount !== nextState.clickCount
+  }
+  render() {
+    this.renderCount++
+    return (
+      <LayoutBox height={200} style={{ background: 'RosyBrown' }} onClick={() => this.setState((state) => ({ clickCount: state.clickCount + 1 }))}>
+        Content Click count : {this.state.clickCount} <br />
+        Content Render count : {this.renderCount} <br />
+        Locale Render count : <RenderCounter />
+      </LayoutBox>
+    )
+  }
+}
+
+/**
+ * Advanced
+ */
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+    this.renderCount = 0
+    this.onResize = throttle(this.onResize.bind(this), 20)
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize)
+  }
+
+  render() {
+    this.renderCount++
+    return (
+      <LayoutContainer width={this.state.width} height={this.state.height} style={{ background: 'MistyRose' }}>
+        <LayoutBox height={50}>
+          App Render count : {this.renderCount} <br />
+        </LayoutBox>
+        <LayoutBox top={50}>
+          <Content />
+        </LayoutBox>
+      </LayoutContainer>
+    )
+  }
+
+  onResize() {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight    
+    })
+  }
+}
 
 export default App
