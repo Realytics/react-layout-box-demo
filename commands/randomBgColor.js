e4db76747aa05e1372b422e0804ef67541cf5e43
Etienne Dldc <etienne.dldc@outlook.fr>
2017-04-22 22:01:19 +0200

    Step 01

--- commands/randomBgColor.js
+++ commands/randomBgColor.js
@@ -1,5 +1,5 @@
 exports.execute = function (args) {
   var insertHere = require('./insertHere')
   var range = (fromVal, toVal) => Math.floor(fromVal + (Math.random() * (toVal - fromVal)))
-  insertHere(args, `style={{ background: \`hsl(${range(0, 360)}, ${range(30, 60)}%, ${range(30, 60)}%)\` }}`)
+  insertHere(args, `style={{ background: \`hsl(${range(0, 360)}, 25%, 50%)\` }}`)
 }
