8c9cacd95d8030d4a1f120901bd88c28005377da
Etienne Dldc <etienne.dldc@outlook.fr>
2017-04-22 22:04:57 +0200

    Reset Step 0

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React from 'react'
-import { LayoutContainer, LayoutBox} from './LayoutBox'
 
 const App = () => (
-  <LayoutContainer width={window.innerWidth} height={window.innerHeight}>
-    <LayoutBox height={70} style={{ background: 'RosyBrown' }}>TopBar</LayoutBox>
-    <LayoutBox top={70}>
-      <LayoutBox width={200} style={{ background: 'BurlyWood' }}>Left Menu</LayoutBox>
-      <LayoutBox left={200} style={{ background: 'MistyRose' }}>
-        {(layout) => (
-          <pre>
-            {JSON.stringify(layout, null, 2)}
-          </pre>
-        )}
-      </LayoutBox>
-    </LayoutBox>
-  </LayoutContainer>
+  <h1>Hello Meetup React !</h1>
 )
 
 export default App
