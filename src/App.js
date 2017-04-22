af74fc02d43382ab72dc81ef2da58ef2ac55f2de
Etienne Dldc <etienne.dldc@outlook.fr>
2017-04-22 22:29:29 +0200

    Style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { LayoutContainer, LayoutBox, LayoutUtils } from './LayoutBox'
 
+const boxStyle = (lighness) => ({ background: `hsl(200, 41%, ${lighness}%)`, padding: 10, fontSize: '2em' })
+
 const App = () => (
   <LayoutContainer width={window.innerWidth} height={window.innerHeight}>
-    <LayoutBox height={70} style={{ background: 'RosyBrown' }}>TopBar</LayoutBox>
+    <LayoutBox height={70} style={boxStyle(60)}>Top Bar</LayoutBox>
     <LayoutBox top={70}>
-      <LayoutBox width={LayoutUtils.percent(50)} style={{ background: 'MistyRose' }}>Left Menu</LayoutBox>
+      <LayoutBox width={LayoutUtils.percent(30)} style={boxStyle(70)}>Left Menu</LayoutBox>
+      <LayoutBox left={LayoutUtils.percent(30)} style={boxStyle(50)}>Content</LayoutBox>
     </LayoutBox>
   </LayoutContainer>
 )
