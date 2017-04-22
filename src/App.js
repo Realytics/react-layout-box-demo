aabcef5d947a8f097b7f5aa24bf31cc2013b2017
Etienne Dldc <etienne.dldc@outlook.fr>
2017-04-22 22:57:44 +0200

    Minor refacto

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import { LayoutContainer, LayoutBox} from './LayoutBox'
 
 const boxStyle = (lighness) => ({ background: `hsl(100, 41%, ${lighness}%)`, padding: 10, fontSize: '2rem' })
 
+const functionAsChildren = (layout) => (
+  <div>
+    Content
+    <pre>
+      {JSON.stringify(layout, null, 2)}
+    </pre>
+  </div>
+)
+
 const App = () => (
   <LayoutContainer width={window.innerWidth} height={window.innerHeight}>
     <LayoutBox height={70} style={boxStyle(50)}>Top Bar</LayoutBox>
     <LayoutBox top={70}>
       <LayoutBox width={200} style={boxStyle(60)}>Left Menu</LayoutBox>
       <LayoutBox left={200} style={boxStyle(70)} verticalScroll>
-        {(layout) => (
-          <div>
-            Content  
-            <pre>  
-              {JSON.stringify(layout, null, 2)}
-            </pre>
-          </div>
-        )}
+        {functionAsChildren}
       </LayoutBox>
     </LayoutBox>
   </LayoutContainer>
