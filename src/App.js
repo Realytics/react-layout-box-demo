79d56febe43dac9e1fdb753e6f124db4abab1b47
Etienne Dldc <etienne.dldc@outlook.fr>
2017-04-22 22:03:41 +0200

    Fix colors

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { LayoutContainer, LayoutBox} from './LayoutBox'
 
 const App = () => (
   <LayoutContainer width={window.innerWidth} height={window.innerHeight}>
-    <LayoutBox height={70} style={{ background: 'DodgerBlue' }}>TopBar</LayoutBox>
+    <LayoutBox height={70} style={{ background: 'RosyBrown' }}>TopBar</LayoutBox>
     <LayoutBox top={70}>
-      <LayoutBox width={200} style={{ background: 'OrangeRed' }}>Left Menu</LayoutBox>
-      <LayoutBox left={200} style={{ background: 'BlueViolet' }}>
+      <LayoutBox width={200} style={{ background: 'BurlyWood' }}>Left Menu</LayoutBox>
+      <LayoutBox left={200} style={{ background: 'MistyRose' }}>
         {(layout) => (
           <pre>
             {JSON.stringify(layout, null, 2)}
