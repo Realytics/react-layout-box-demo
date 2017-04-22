eb0e54f53d16b1f9a986a6ce5ee1d5a6b1fb5eec
Etienne Dldc <etienne.dldc@outlook.fr>
2017-04-22 22:01:19 +0200

    Step 01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { LayoutContainer, LayoutBox } from './LayoutBox'
 
 const App = () => (
-  <h1>Hello Meetup React !</h1>
+  <LayoutContainer width={window.innerWidth} height={window.innerHeight}>
+    <LayoutBox height={70} style={{ background: 'RosyBrown' }}>Top Bar</LayoutBox>
+    <LayoutBox top={70}>
+      <LayoutBox width={250} style={{ background: 'BurlyWood' }}>Left Menu</LayoutBox>
+      <LayoutBox left={250} style={{ background: 'MistyRose' }}>Content</LayoutBox>
+    </LayoutBox>
+  </LayoutContainer>
 )
 
 export default App
