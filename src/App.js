075d80ea1c41825fb9240a207b9dd193a2b31dd1
Etienne Dldc <etienne.dldc@outlook.fr>
2017-04-22 22:28:06 +0200

    Style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { LayoutContainer, LayoutBox } from './LayoutBox'
+
+const boxStyle = (lighness) => ({ background: `hsl(100, 41%, ${lighness}%)`, padding: 10, fontSize: '2em' })
 
 const App = () => (
-  <h1>Hello Meetup React !</h1>
+  <LayoutContainer width={window.innerWidth} height={window.innerHeight}>
+    <LayoutBox height={70} style={boxStyle(50)}>TopBar</LayoutBox>
+    <LayoutBox top={70}>
+      <LayoutBox width={200} style={boxStyle(60)}>LeftMenu</LayoutBox>
+      <LayoutBox left={200} style={boxStyle(70)} verticalScroll>
+        {(layout) => (
+          <div>
+            Content  
+            <pre>  
+              {JSON.stringify(layout, null, 2)}
+            </pre>
+          </div>
+        )}
+      </LayoutBox>
+    </LayoutBox>
+  </LayoutContainer>
 )
 
 export default App
