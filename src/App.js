efb1895286a3b9aaeb59e902512ab26f8e9da468
Etienne Dldc <etienne.dldc@outlook.fr>
2017-04-22 22:06:23 +0200

    Step 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { LayoutContainer, LayoutBox, InjectLayout } from './LayoutBox'
+
+const StupidComponent = (props) => (
+  <p>{`${props.layout.width.value} x ${props.layout.height.value}`}</p>
+)
+const StupidComponentWithLayout = InjectLayout(StupidComponent)
 
 const App = () => (
-  <h1>Hello Meetup React !</h1>
+  <LayoutContainer width={window.innerWidth} height={window.innerHeight}>
+    <LayoutBox height={70} style={{ background: 'DodgerBlue' }}>TopBar</LayoutBox>
+    <LayoutBox top={70}>
+      <LayoutBox width={200} style={{ background: 'OrangeRed' }}>Left Menu</LayoutBox>
+      <LayoutBox left={200} style={{ background: 'BlueViolet' }}>
+        <StupidComponentWithLayout />
+      </LayoutBox>
+    </LayoutBox>
+  </LayoutContainer>
 )
 
 export default App
