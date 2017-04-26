bd6896d466c367a833bc1878047d7bd8e49e5187
Etienne Dldc <etienne.dldc@outlook.fr>
2017-04-27 01:16:24 +0200

    Message List

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React from 'react'
 
+const Button = (props) => (
+  <button style={{background: props.color}}>
+    {props.children}
+  </button>
+)
+
+const Message = (props) => (
+  <div>
+    {props.text} <Button color={props.color}>Delete</Button>
+  </div>
+)
+
+class MessageList extends React.Component {
+  render() {
+    const color = "PapayaWhip"
+    return (
+      <div>
+        {React.Children.toArray(
+          this.props.messages.map((message) =>
+            <Message text={message.text} color={color} />
+          )
+        )}
+      </div>
+    )
+  }
+}
+
+const messages = [
+  { text: 'React' },
+  { text: 'Redux' },
+  { text: 'Typescript' },
+]
+
 const App = () => (
-  <h1>Hello Meetup React !</h1>
+  <MessageList messages={messages} />
 )
 
 export default App
